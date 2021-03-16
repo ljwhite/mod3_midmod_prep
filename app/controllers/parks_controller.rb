b5716805d03def054dee446046efdba511062420
Lito White <leland.white@gmail.com>
2021-03-15 18:34:09 -0600

    updated for completion

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -2,16 +2,16 @@ class ParksController < ApplicationController
   def index
     # state = params[:state]
     # @parks = call(state)
-
-
+    #
+    #
     #
     # def conn
     #     @conn ||= Faraday.new(url: "https://developer.nps.gov/api/v1") do |faraday|
     #       farday.headers["X-API-KEY"] = ENV['PARKS_FINDER_API-KEY']
     #   end
     # end
-
-
+    #
+    #
     # response = conn.get("/api/v1/parks?stateCode=#{state}")
     # json = JSON.parse(response.body, symbolize_names: true)
     # @parks = json[:data].map do |park|
--- spec/requests/user_story_spec.rb
+++ spec/requests/user_story_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-feature "user can search for house " do
+feature "user can search for parks " do
   scenario "user submits state name" do
     visit '/'
     select "Tennessee", from: :state
     click_on "Find Parks"
     expect(current_path).to eq(parks_path)
-    expect(page).to have_content("16 Parks")
-    expect(page).to have_css(".park", count: 16)
+    expect(page).to have_content("15 Parks")
+    expect(page).to have_css(".park", count: 15)
 
     within(first(".park")) do
       expect(page).to have_css(".name")
@@ -15,6 +15,5 @@ feature "user can search for house " do
       expect(page).to have_css(".directions")
       expect(page).to have_css(".hours")
     end
-    save_and_open_page
   end
 end
